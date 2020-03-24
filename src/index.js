087733f5b973017aa112bcbd7842f7a4e943ae2b
Michael Skeen <mmskeen@gmail.com>
2020-03-24 16:50:41 -0600

    Completed simple list in JSX

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Pizza</li>
+      <li>Lasagne</li>
+      <li>Fried Chicken</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
